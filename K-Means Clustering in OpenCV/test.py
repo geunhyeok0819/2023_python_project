92aa1cb8864a4a19f76a6599b691a9452ed9d873
geunhyeok0819 <geunhyeok0819@gmail.com>
2023-08-13 15:58:54 +0900

    :memo:update

--- K-Means Clustering in OpenCV/test.py
+++ K-Means Clustering in OpenCV/test.py
@@ -7,9 +7,8 @@ def detectAndDisplay(frame):
     #-- Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
     for (x,y,w,h) in faces:
-        center = (x, y)
-        end = (x+w,y+h)
-        frame = cv.rectangle(frame, center, end,(0, 0, 0), -100)
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
         faceROI = frame_gray[y:y+h,x:x+w]
     
         #-- In each face, detect eyes
@@ -18,7 +17,7 @@ def detectAndDisplay(frame):
             eye_center = (x + x2 + w2//2, y + y2 + h2//2)
             radius = int(round((w2 + h2)*0.25))
     
-        
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
     
     cv.imshow('Capture - Face detection', frame)
 
