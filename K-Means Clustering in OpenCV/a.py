af5783da3d2fe500b49dcceabe052f4eefe4fd7d
geunhyeok0819 <geunhyeok0819@gmail.com>
2023-09-24 15:57:43 +0900

    :memo:update

--- K-Means Clustering in OpenCV/a.py
+++ K-Means Clustering in OpenCV/a.py
@@ -2,8 +2,8 @@ from __future__ import print_function
 import cv2 as cv
 import argparse
 
-def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+def detectAndDisplay(frame): # 얼굴을 감지하고 화면에 표시한다
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #색상 전환
     frame_gray = cv.equalizeHist(frame_gray)
     #-- Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
@@ -23,16 +23,11 @@ parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='./h
 parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
 args = parser.parse_args()
 face_cascade_name = args.face_cascade
-eyes_cascade_name = args.eyes_cascade
 face_cascade = cv.CascadeClassifier()
-eyes_cascade = cv.CascadeClassifier()
 
 #-- 1. Load the cascades
 if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
-    exit(0)
-if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-    print('--(!)Error loading eyes cascade')
     exit(0)
 
 camera_device = args.camera
